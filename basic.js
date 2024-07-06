a6b291725eb444934042ff5d3aaf1b5ae789acfd
Aniket7291 <115251050+Aniket7291@users.noreply.github.com>
2024-07-06 23:20:35 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,45 @@
+// let obj1={
+//     name:"gaurav",
+//     batch:2025,
+//     section:"AI-A",
+//     isStudent:true,
+//     marks:[100,98,95],
+//     "course name":"btech",
+// };
+// // let obj2= new Object();
+// // // console.log(obj1.name);
+// // // console.log(obj1["name"]);
+// // // console.log(obj1["course name "]);
+// // obj1.name="ABESIT";
+// // // console.log(obj1);
+// // obj1.project="js";
+
+// obj1.func=function(){
+//     console.log("HI I AM FUNCTION IN OBJECT");
+
+// }
+
+// // console.log(obj1.func())
+// // let obj1={1:"a",2:"b"};
+// let obj2={3:"c",4:"d"};
+
+// let obj3=Object.assign(obj1,obj2);
+// console.log(obj3);
+
+// let obj4={...obj1,...obj2};
+// console.log(obj4);
+
+// const {a,b}=obj1;
+
+// // const{"batch"}=obj1;
+// // console.log(batch);
+
+
+// // object destructing --  the name of the variable in destructe object must be same as the name of the key is the object 
+// // const{keyname,kayname}=objectname
+// const{batch,func}=obj1;
+// console.log(batch);
+// console.log(func());
+
+
+
